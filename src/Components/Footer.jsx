1133662bad054d4a0fd2acd9cc4054dce6a947d1
Robby94gtr <robertogramazio94@gmail.com>
2025-05-11 22:56:19 +0200

    [UPD] Added Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    return(
+        <div className="Footer-image">
+
+            <div className="Footer-logo">
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Footer;
