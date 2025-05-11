2ed68685a22e5cc2316112c0c366084fb96cf1bc
Robby94gtr <robertogramazio94@gmail.com>
2025-05-11 23:06:38 +0200

    [UPD] Added Footer List

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 import "./App.css";
 
 function App(){
@@ -7,6 +8,7 @@ function App(){
     <>
  <Navbar />
  <Main />
+ <Footer/>
   </>
 
 
