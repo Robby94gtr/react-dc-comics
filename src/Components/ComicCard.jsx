6863b80555e08e978386963e5d99dfc3a95e95b1
Robby94gtr <robertogramazio94@gmail.com>
2025-05-12 10:31:55 +0200

    Finished

--- src/Components/ComicCard.jsx
+++ src/Components/ComicCard.jsx
@@ -1,11 +1,17 @@
+// ComicCard.jsx
 import React from 'react'
 
-const ComicCard = () => {
+const ComicCard = ({ title, image }) => {
     return (
-        <div>
+        <div className="comic-card">
+            <img src={image} alt={title} />
+            <div className="card-info">
+                <h2>{title}</h2>
+
+            </div>
 
         </div>
-    )
-}
+    );
+};
 
 export default ComicCard;
