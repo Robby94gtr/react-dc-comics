39efcee68be9e5a3180606a6d23a06e3f180444b
Robby94gtr <robertogramazio94@gmail.com>
2025-05-12 10:17:29 +0200

    [UPD] Added Array

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
+// src/App.jsx
+
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
+import ListComics from "./Components/ListComics";
 import Footer from "./Components/Footer";
 import "./App.css";
 
-function App(){
-  return(
-    <>
- <Navbar />
- <Main />
- <Footer/>
-  </>
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
 
+      <Main />
 
-
+      <Footer />
+    </div>
   );
 }
 
